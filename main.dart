e8d64dd5ce2fbc55340acf20563b0587eaea2bff
Jeffman <jeffman@JeffmandeMacBook-Pro.local>
2022-05-10 15:50:43 +0800

    feat: add.two files

---
+++ main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:gitown/route_path.dart';
+import 'package:mmkv/mmkv.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+import 'com/camp/log/log.dart';
+import 'com/camp/page/home/home_page.dart';
+import 'com/camp/page/login/login_page.dart';
+import 'com/camp/user/app_user.dart';
+import 'com/camp/widget/no_scroll_behavior.dart';
+
+void main() async {
+  await MMKV.initialize();
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = 'https://a5137591199344178b7dc98b0fc871f0@o1220400.ingest.sentry.io/6363311';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(MyApp()),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Git.me',
+      builder: (context, child) {
+        return ScrollConfiguration(
+          behavior: NoScrollBehavior(),
+          child: child ?? const Spacer(),
+        );
+      },
+      theme: ThemeData(primarySwatch: Colors.green, scaffoldBackgroundColor: Colors.grey[50]),
+      initialRoute: AppUser().isLogin() ? HomePage.PATH : LoginPage.PATH,
+      onGenerateRoute: (setting) {
+        final name = setting.name;
+        final Function? pageBuilder = RoutePath.routes[name];
+        Log().d("name", name);
+        if (pageBuilder != null) {
+          if (setting.arguments != null) {
+            return MaterialPageRoute(builder: (context) {
+              Log().d("arguments", setting.arguments);
+              return pageBuilder(context, arguments: setting.arguments);
+            });
+          } else {
+            return MaterialPageRoute(builder: (context) => pageBuilder(context));
+          }
+        }
+      },
+    );
+  }
+}
---
+++ route_path.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+import 'com/camp/page/home/home_page.dart';
+import 'com/camp/page/home/qrcode_page.dart';
+import 'com/camp/page/issue/issue_assignee_select_page.dart';
+import 'com/camp/page/issue/issue_detail_page.dart';
+import 'com/camp/page/login/login_page.dart';
+import 'com/camp/page/repos/file_detail_page.dart';
+import 'com/camp/page/repos/repos_contributor_page.dart';
+import 'com/camp/page/repos/repos_detail_page.dart';
+import 'com/camp/page/repos/repos_fork_page.dart';
+import 'com/camp/page/repos/repos_release_page.dart';
+import 'com/camp/page/repos/repos_search_page.dart';
+import 'com/camp/page/repos/repos_stargazer_page.dart';
+import 'com/camp/page/repos/repos_watcher_page.dart';
+import 'com/camp/page/search/search_page.dart';
+import 'com/camp/page/setting/about_page.dart';
+import 'com/camp/page/setting/setting_page.dart';
+import 'com/camp/page/issue/commit_list_page.dart';
+import 'com/camp/page/issue/commit_message_page.dart';
+import 'com/camp/page/issue/create_comment_page.dart';
+import 'com/camp/page/issue/create_issue_page.dart';
+import 'com/camp/page/issue/edit_issue_page.dart';
+import 'com/camp/page/issue/issue_labels_select_page.dart';
+import 'com/camp/page/issue/issue_milestone_select_page.dart';
+import 'com/camp/page/issue/my_issue_page.dart';
+import 'com/camp/page/pull/my_pull_page.dart';
+import 'com/camp/page/pull/pull_detail_page.dart';
+import 'com/camp/page/pull/pull_dismiss_review.dart';
+import 'com/camp/page/pull/pull_merge_option.dart';
+import 'com/camp/page/pull/pull_review_select_page.dart';
+import 'com/camp/page/issue/repos_issue_page.dart';
+import 'com/camp/page/issue/repos_pull_page.dart';
+import 'com/camp/page/issue/test_page.dart';
+import 'com/camp/page/login/login_web_page.dart';
+import 'com/camp/page/pull/commit_file_detail.dart';
+import 'com/camp/page/repos/repos_code_page.dart';
+import 'com/camp/page/repos/repos_file_page.dart';
+import 'com/camp/page/search/search_history_page.dart';
+import 'com/camp/page/setting/accounts_page.dart';
+import 'com/camp/page/setting/feedback_page.dart';
+import 'com/camp/page/user/org_detail_page.dart';
+import 'com/camp/page/user/org_member_page.dart';
+import 'com/camp/page/user/slug_repos_page.dart';
+import 'com/camp/page/user/me_profile_page.dart';
+import 'com/camp/page/user/notification_list_page.dart';
+import 'com/camp/page/user/org_list_page.dart';
+import 'com/camp/page/user/user_detail_page.dart';
+import 'com/camp/page/user/user_follower_page.dart';
+import 'com/camp/page/user/user_following_page.dart';
+import 'com/camp/page/user/user_repos_page.dart';
+import 'com/camp/page/user/user_star_page.dart';
+import 'com/camp/page/web/web_page.dart';
+
+class RoutePath {
+  static var routes = {
+    AboutPage.PATH: (context) => const AboutPage(),
+    LoginPage.PATH: (context, {arguments}) => LoginPage(arguments: arguments),
+    LoginWebPage.PATH: (context, {arguments}) => LoginWebPage(arguments: arguments),
+    MeProfilePage.PATH: (context) => const MeProfilePage(),
+    AccountsPage.PATH: (context) => AccountsPage(),
+    FeedbackPage.PATH: (context) => FeedbackPage(),
+    HomePage.PATH: (context) => const HomePage(),
+    TestPage.PATH: (context) => const TestPage(),
+    QRCodePage.PATH: (context) => const QRCodePage(),
+    SettingPage.PATH: (context) => const SettingPage(),
+    WebPage.PATH: (context, {arguments}) => WebPage(arguments),
+    SearchHistoryPage.PATH: (context) => const SearchHistoryPage(),
+    NotificationListPage.PATH: (context) => NotificationListPage(),
+    SearchPage.PATH: (context, {arguments}) => SearchPage(arguments),
+    MyIssuePage.PATH: (context, {arguments}) => MyIssuePage(arguments),
+    MyPullPage.PATH: (context, {arguments}) => MyPullPage(arguments),
+    UserStarPage.PATH: (context, {arguments}) => UserStarPage(arguments),
+    UserReposPage.PATH: (context, {arguments}) => UserReposPage(arguments),
+    UserDetailPage.PATH: (context, {arguments}) => UserDetailPage(arguments),
+    OrganizationDetailPage.PATH: (context, {arguments}) => OrganizationDetailPage(arguments),
+    UserFollowerPage.PATH: (context, {arguments}) => UserFollowerPage(arguments),
+    UserFollowingPage.PATH: (context, {arguments}) => UserFollowingPage(arguments),
+    ReposCodePage.PATH: (context, {arguments}) => ReposCodePage(arguments),
+    ReposFilePage.PATH: (context, {arguments}) => ReposFilePage(arguments),
+    ReposIssuePage.PATH: (context, {arguments}) => ReposIssuePage(arguments),
+    IssueDetailPage.PATH: (context, {arguments}) => IssueDetailPage(arguments),
+    ReposDetailPage.PATH: (context, {arguments}) => ReposDetailPage(arguments),
+    ReposSearchPage.PATH: (context, {arguments}) => ReposSearchPage(arguments),
+    EditIssuePage.PATH: (context, {arguments}) => EditIssuePage(arguments),
+    CreateIssuePage.PATH: (context, {arguments}) => CreateIssuePage(arguments),
+    CreateCommentPage.PATH: (context, {arguments}) => CreateCommentPage(arguments),
+    ReposPullPage.PATH: (context, {arguments}) => ReposPullPage(arguments),
+    PullDetailPage.PATH: (context, {arguments}) => PullDetailPage(arguments),
+    OrganizationListPage.PATH: (context, {arguments}) => OrganizationListPage(arguments),
+    IssueLabelsSelectPage.PATH: (context, {arguments}) => IssueLabelsSelectPage(arguments),
+    IssueAssigneesSelectPage.PATH: (context, {arguments}) => IssueAssigneesSelectPage(arguments),
+    IssueMilestoneSelectPage.PATH: (context, {arguments}) => IssueMilestoneSelectPage(arguments),
+    PullReviewSelectPage.PATH: (context, {arguments}) => PullReviewSelectPage(arguments),
+    PullDismissReviewPage.PATH: (context, {arguments}) => PullDismissReviewPage(arguments),
+    PullMergeOption.PATH: (context, {arguments}) => PullMergeOption(arguments),
+    CommitMessagePage.PATH: (context, {arguments}) => CommitMessagePage(arguments),
+    CommitListPage.PATH: (context, {arguments}) => CommitListPage(arguments),
+    CommitFilePage.PATH: (context, {arguments}) => CommitFilePage(arguments),
+    ReposWatcherPage.PATH: (context, {arguments}) => ReposWatcherPage(arguments),
+    ReposForkPage.PATH: (context, {arguments}) => ReposForkPage(arguments),
+    ReposStargazerPage.PATH: (context, {arguments}) => ReposStargazerPage(arguments),
+    ReposContributorPage.PATH: (context, {arguments}) => ReposContributorPage(arguments),
+    FileDetailPage.PATH: (context, {arguments}) => FileDetailPage(arguments),
+    SlugReposPage.PATH: (context, {arguments}) => SlugReposPage(arguments),
+    ReposReleasePage.PATH: (context, {arguments}) => ReposReleasePage(arguments),
+    OrgMemberPage.PATH: (context, {arguments}) => OrgMemberPage(arguments),
+  };
+
+  static handleUrl(BuildContext context, String? url, {String? title}) {
+    if (url?.isNotEmpty != true) return;
+    print("handleUrl:$url");
+    try {
+      Uri uri = Uri.parse(url ?? "");
+      if (uri.host == "api.github.com" || uri.host == "github.com") {
+        if (uri.pathSegments.contains("issues")) {
+          int index = uri.pathSegments.indexOf("issues");
+          String reposFullName = "${uri.pathSegments[0]}/${uri.pathSegments[1]}";
+          if (index == uri.pathSegments.length - 1) {
+            ReposIssuePage.launch(context, reposFullName);
+          } else {
+            String issueNumber = uri.pathSegments[index + 1];
+            IssueDetailPage.launch(context, reposFullName, int.parse(issueNumber));
+          }
+        } else if (uri.pathSegments.contains("pulls")) {
+          String reposFullName = "${uri.pathSegments[0]}/${uri.pathSegments[1]}";
+          ReposPullPage.launch(context, reposFullName);
+        } else if (uri.pathSegments.contains("pull")) {
+          int index = uri.pathSegments.indexOf("pull");
+          String reposFullName = "${uri.pathSegments[0]}/${uri.pathSegments[1]}";
+          String issueNumber = uri.pathSegments[index + 1];
+          PullDetailPage.launch(context, reposFullName, int.parse(issueNumber));
+        } else if (uri.pathSegments.contains("releases")) {
+          String reposFullName = "${uri.pathSegments[0]}/${uri.pathSegments[1]}";
+          ReposReleasePage.launch(context, reposFullName);
+        } else if (uri.pathSegments.length == 2) {
+          ReposDetailPage.launch(context, "${uri.pathSegments[0]}/${uri.pathSegments[1]}");
+        } else if (uri.pathSegments.length == 1) {
+          UserDetailPage.launch(context, uri.pathSegments[0]);
+        }else {
+          String reposFullName = "${uri.pathSegments[0]}/${uri.pathSegments[1]}";
+          ReposDetailPage.launch(context, reposFullName);
+        }
+      } else {
+        WebPage.launch(context, url, title: title);
+      }
+    } catch (e) {
+      WebPage.launch(context, url, title: title);
+    }
+  }
+}
